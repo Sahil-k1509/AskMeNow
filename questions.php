64435c85b77afa74066e9c5c38e5286a0e2b4962
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-06-27 23:53:45 +0530

    Added questions for the quiz

---
+++ questions.php
@@ -0,0 +1,43 @@
+<?php 
+
+$easyLevel = array(
+    array("level"=>"Easy","question"=>"How are distances associated with planets measured?", "options"=>array("With a long ruler", "Don't know", "Who cares", "Angle subtended by planet") ),
+    array("level"=>"Easy","question"=>"What is the best method for exam?", "options"=>array("Online", "Offline", "No exams", "Both") ),
+    array("level"=>"Easy","question"=>"Which of the languages has Goroutines?", "options"=>array("Python", "Java", "Golang", "Kotlin") ),
+    array("level"=>"Easy","question"=>"What is boiling point of water?", "options"=>array("100 C", "273 K", "Depends on air pressure", "Very High") ),
+    array("level"=>"Easy","question"=>"Have you liked the page of iit kgp memers society?", "options"=>array("Yes", "No, but I will", "What is that?", "I am from Comedy Club!") )
+);
+$EasyAnswers = array("4", "3", "3", "3", "1");
+
+
+$MediumLevel = array(
+    array("level"=>"Medium","question"=>"What is the first name of Khalifa?", "options"=>array("Burj", "Wiz", "Mia", "No idea") ),
+    array("level"=>"Medium","question"=>"What kind of language is Python?", "options"=>array("Compiled", "Interpreted", "Markup", "It is a snake") ),
+    array("level"=>"Medium","question"=>"Which of the following is not a valid keyword in javascript?", "options"=>array("var", "let", "func", "import") ),
+    array("level"=>"Medium","question"=>"What is 20*6? ", "options"=>array("119.998", "5!", "16.667", "69") ),
+    array("level"=>"Medium","question"=>"Which of the following is not a programming language?", "options"=>array("Go", "Rust", "R", "HTML") )
+);
+$MediumAnswers = array("1", "2", "3", "2", "4");
+
+
+$HardLevel = array(
+    array("level"=>"Hard","question"=>"Which of the following is given high priority in CSS?", "options"=>array("class", "id", "element", "They don't matter") ),
+    array("level"=>"Hard","question"=>"For which of the following options, all parts are indentation sensitive?", "options"=>array("Python, CSS, C++", "Pug, Javascript, C", "SASS, Pug, python", "PHP, Perl, CSS") ),
+    array("level"=>"Hard","question"=>"What is the broadcast address?", "options"=>array("127.0.0.1", "192.44.35.6", "255.255.255.255" ,"127.255.255.0") ),
+    array("level"=>"Hard","question"=>"In 192.168.0.0:69, What does :69 represent?", "options"=>array("port", "socket address", "unicast", "host ID") ),
+    array("level"=>"Hard","question"=>"What is Tkinter library primarily used for in python?", "options"=>array("Macheen Larning", "Web development", "App development", "GUI development") )
+);
+$HardAnswers = array("2", "3", "3", "1", "4");
+
+
+$ExtremeLevel = array(
+    array("level"=>"Extreme","question"=>"How many layers are there in a TCP/IP model?", "options"=>array("2", "5", "9", "0") ),
+    array("level"=>"Extreme","question"=>"Which is not a Structured Query Language?", "options"=>array("MySQL", "Postgres", "MongoDB", "MariaDB") ),
+    array("level"=>"Extreme","question"=>"What does 'C' in 'MVC' stand for?", "options"=>array("Command", "Controller", "Creator", "Catch") ),
+    array("level"=>"Extreme","question"=>"Which of the following is not a framework of python?", "options"=>array("Django", "Flask", "Hug", "Spring") ),
+    array("level"=>"Extreme","question"=>"Which of the following is not a pattern matching Algorithm?", "options"=>array("Boyer Moore", "Rabin Karp", "Knuth Morris Pratt", "Seive of Eratosthenes") )
+);
+$ExtremeAnswers = array("2", "3", "2", "4", "4");
+
+
+?>
