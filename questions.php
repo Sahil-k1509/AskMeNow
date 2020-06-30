06b857895c2bce7dda7aa9baaa705b248dab9c54
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-06-30 21:48:46 +0530

    Added more questions

--- questions.php
+++ questions.php
@@ -1,23 +1,41 @@
 <?php 
 
 $easyLevel = array(
-    array("level"=>"Easy","question"=>"How are distances associated with planets measured?", "answer"=>"4", "options"=>array("With a long ruler", "Don't know", "Who cares", "Angle subtended by planet") ),
+    array("level"=>"Easy","question"=>"To which of the following person, Rules of physics don't apply like everyone else?", "answer"=>"4", "options"=>array("Rohit Shitty", "Selmon Bhoi", "Both are bounded by physics", "Both have defied all the rules of physics") ),
     array("level"=>"Easy","question"=>"What is the best method for exam?", "answer"=>"3", "options"=>array("Online", "Offline", "No exams", "Both") ),
-    array("level"=>"Easy","question"=>"Which of the languages has Goroutines?", "answer"=>"3", "options"=>array("Python", "Java", "Golang", "Kotlin") ),
+    array("level"=>"Easy","question"=>"What is the meaning of quanta?", "answer"=>"1", "options"=>array("Discrete", "Having high energy", "Formless", "Continuous") ),
+    array("level"=>"Easy","question"=>"What is equivalent equation of F = ma?", "answer"=>"1", "options"=>array("F = dp/dt", "F = mc^2", "F = LPU", "All of these") ),
     array("level"=>"Easy","question"=>"What is boiling point of water?", "answer"=>"3", "options"=>array("100 C", "273 K", "Depends on air pressure", "Very High") ),
+    array("level"=>"Easy","question"=>"How do you put a single line comment in python?", "answer"=>"2", "options"=>array("//", "#", "::", "[Hide]") ),
+    array("level"=>"Easy","question"=>"What is LAN?", "answer"=>"3", "options"=>array("Line Area Network", "Listed Anonymous Network", "Local Area Network", "Level Approximation Nuisance") ),
+    array("level"=>"Easy","question"=>"What is the charge of an electron?", "answer"=>"4", "options"=>array("Alternate between -1e and +1e", "+1e", "Zero", "Negative") ),
+    array("level"=>"Easy","question"=>"If two trains start towardsd each other with speed of 75km/hr and 60 km/hr and meet at the mid point of their starting points, Which is closer to the starting point of train A?", "answer"=>"1", "options"=>array("Both", "Train A", "Train B", "Information insufficient") ),
+    array("level"=>"Easy","question"=>"Describe the path of a projectile?", "answer"=>"2", "options"=>array("Straight line", "Parabolic", "Helical", "Circular") ),
+    array("level"=>"Easy","question"=>"How does a senior go on a date?", "answer"=>"2", "options"=>array("Asking out their crush", "Take juniors on treat, pretending it's a date", "After being best friend Zoned", "They don't go on a date") ),
+    array("level"=>"Easy","question"=>"What is the smallest length that we can possibly measure?", "answer"=>"3", "options"=>array("Stephan's Length", "Einstein's Distance", "Plank's Length", "Modi metre") ),
+    array("level"=>"Easy","question"=>"Which of the languages has Goroutines?", "answer"=>"3", "options"=>array("Python", "Java", "Golang", "Kotlin") ),
+    array("level"=>"Easy","question"=>"What will be the outcome of calling someone while they are playing pubg?", "answer"=>"4", "options"=>array("A lot of abuses", "No Chicken in dinner", "Broken teeth", "All of the above") ),
     array("level"=>"Easy","question"=>"Have you liked the page of iit kgp memers society?", "answer"=>"1", "options"=>array("Yes", "No, but I will", "What is that?", "I am from Comedy Club!") )
 );
-//$EasyAnswers = array("4", "3", "3", "3", "1");
 
 
 $MediumLevel = array(
     array("level"=>"Medium","question"=>"What is the first name of Khalifa?", "answer"=>"1", "options"=>array("Burj", "Wiz", "Mia", "No idea") ),
     array("level"=>"Medium","question"=>"What kind of language is Python?", "answer"=>"2", "options"=>array("Compiled", "Interpreted", "Markup", "It is a snake") ),
     array("level"=>"Medium","question"=>"Which of the following is not a valid keyword in javascript?", "answer"=>"3", "options"=>array("var", "let", "func", "import") ),
+    array("level"=>"Medium","question"=>"Having both particle and wave properties is also called as? ", "answer"=>"2", "options"=>array("Mixing", "Wave Particle duality", "Entanglement", "Confusion") ),
+    array("level"=>"Medium","question"=>"Which principle states that we can't determine velocity and position simultaneously? ", "answer"=>"1", "options"=>array("Heisenberg's Uncertainty Principle", "Newton's Indeterminate Principle", "Schrodinger's Confusion Theorem", "Velocity Coordinate Rule") ),
+    array("level"=>"Medium","question"=>"If two particles are entangled and one has it's spin up, What is the spin state of other particle? ", "answer"=>"2", "options"=>array("Up", "Down", "No spin", "Spin of both particles are independent") ),
+    array("level"=>"Medium","question"=>"Which of the following is radioactive? ", "answer"=>"4", "options"=>array("Mercury", "Gallium", "Lead", "C-14") ),
+    array("level"=>"Medium","question"=>"In quantum mechanics, Angular momentum of particle is also called? ", "answer"=>"3", "options"=>array("Swirl", "Orbital", "Spin", "Centrifuge") ),
+    array("level"=>"Medium","question"=>"Which is not a fundamental force of nature? ", "answer"=>"3", "options"=>array("Strong nulcear force", "Gravity", "Momentum", "Electro Magnetic force") ),
+    array("level"=>"Medium","question"=>"What does 'hello' become on rotating all characters by 2 in clockwise direction? ", "answer"=>"1", "options"=>array("jgnnq", "fcjjm", "zkmme", "olleh") ),
+    array("level"=>"Medium","question"=>"What is JSON? ", "answer"=>"2", "options"=>array("A programming language", "Way of storing data", "An operating system", "Name of Elon Musk's latest supercar") ),
     array("level"=>"Medium","question"=>"What is 20*6? ", "answer"=>"2", "options"=>array("119.998", "5!", "16.667", "69") ),
+    array("level"=>"Medium","question"=>"What is e^(i*pi), where i is square root of -1? ", "answer"=>"4", "options"=>array("2*e^(1/pi^e)", "1", "0", "-1") ),
+    array("level"=>"Medium","question"=>"Which of the following is a pseudo element? ", "answer"=>"3", "options"=>array(":hold", "#before", "::after", "_unreal") ),
     array("level"=>"Medium","question"=>"Which of the following is not a programming language?", "answer"=>"4", "options"=>array("Go", "Rust", "R", "HTML") )
 );
-//$MediumAnswers = array("1", "2", "3", "2", "4");
 
 
 $HardLevel = array(
@@ -25,19 +43,38 @@ $HardLevel = array(
     array("level"=>"Hard","question"=>"For which of the following options, all parts are indentation sensitive?", "answer"=>"3", "options"=>array("Python, CSS, C++", "Pug, Javascript, C", "SASS, Pug, python", "PHP, Perl, CSS") ),
     array("level"=>"Hard","question"=>"What is the broadcast address?", "answer"=>"3", "options"=>array("127.0.0.1", "192.44.35.6", "255.255.255.255" ,"127.255.255.0") ),
     array("level"=>"Hard","question"=>"In 192.168.0.0:69, What does :69 represent?", "answer"=>"1", "options"=>array("port", "socket address", "unicast", "host ID") ),
+    array("level"=>"Hard","question"=>"What kind of error will this program from python give?<br>def function():<br>&nbsp;&nbsp;&nbsp;class ABC():<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;pass<br>&nbsp;&nbsp;&nbsp;return ABC", "answer"=>"3", "options"=>array("Syntax Error", "Indentation Error", "No error", "Invalid return type Error") ),
+    array("level"=>"Hard","question"=>"What is SEO?", "answer"=>"1", "options"=>array("Search Engine Optimization", "Semantic Error Optimization", "Serialize Engine Observation", "Send Encrypted Objects") ),
+    array("level"=>"Hard","question"=>"What is the remainder when 6^5626945 is divided by 35?", "answer"=>"2", "options"=>array("1", "6", "0", "34") ),
+    array("level"=>"Hard","question"=>"Which sorting technique has best runtime complexity", "answer"=>"2", "options"=>array("Bubble sort", "Heap sort", "Random sort", "Insertion sort") ),
+    array("level"=>"Hard","question"=>"In which category will a 'N queen problem' fit most accurately?", "answer"=>"1", "options"=>array("Backtracking", "Greedy", "Pattern Matching", "Sorting") ),
+    array("level"=>"Hard","question"=>"If there are n boxes and n+1 items, one box will have 2 items. This is also known as?", "answer"=>"4", "options"=>array("Box item relationship", "Box deficiency Theorem", "Multiple Stacking Theorem", "Pigeon hole principle") ),
+    array("level"=>"Hard","question"=>"Which of the following is the fastest language?", "answer"=>"2", "options"=>array("Python", "Julia", "Octave", "R") ),
+    array("level"=>"Hard","question"=>"How many bits are there in a MAC address?", "answer"=>"3", "options"=>array("128", "32", "48", "64") ),
+    array("level"=>"Hard","question"=>"Every particle has _____ according to Dirac Equation. What is ____ ?", "answer"=>"2", "options"=>array("Duality", "Antiparticle", "Matrix", "Wave function") ),
+    array("level"=>"Hard","question"=>"First antiparticle to be discovered? ", "answer"=>"2", "options"=>array("Antiproton", "Positron", "Antineutrino", "X-ray") ), 
     array("level"=>"Hard","question"=>"What is Tkinter library primarily used for in python?", "answer"=>"4", "options"=>array("Macheen Larning", "Web development", "App development", "GUI development") )
+    
 );
-//$HardAnswers = array("2", "3", "3", "1", "4");
 
 
 $ExtremeLevel = array(
     array("level"=>"Extreme","question"=>"How many layers are there in a TCP/IP model?", "answer"=>"2", "options"=>array("2", "5", "9", "0") ),
+    array("level"=>"Extreme","question"=>"Which media query series is correct for a 'desktop first' responsive design?", "answer"=>"1", "options"=>array("@media(max-width: 1000px){}<br>@media(max-width: 500px){}", "@media(min-width: 1000px){}<br>@media(min-width: 500px){}", "@media(min-width: 500px){}<br>@media(min-width: 1000px){}", "@media(max-width: 500px){}<br>@media(max-width: 1000px){}") ),
+    array("level"=>"Extreme","question"=>"Which of the following is a Polyalphabetic cipher?", "answer"=>"4", "options"=>array("Keyed Cipher", "Affine Cipher", "Keyless Cipher", "Playfair Cipher") ),
+    array("level"=>"Extreme","question"=>"What is the value of z for which (A&z).(B&z) gives maximum value where A, B > 0. '&' is bitwise And operator and '.' is multiply", "answer"=>"1", "options"=>array("A|B", "A", "B^A", "A^B|((2.A)&B)") ),
+    array("level"=>"Extreme","question"=>"Which of the following is not a valid ternary operator syntax for any language?", "answer"=>"3", "options"=>array("y = condition ? A : B;", "y = A if condition else B", "condition ? y = A : y = B;", "y = if (condition) A else B;") ),
+    array("level"=>"Extreme","question"=>"Which http status code is returned if there is a bad gateway error?", "answer"=>"2", "options"=>array("401", "502", "431", "302") ),
+    array("level"=>"Extreme","question"=>"Which of the following is the correct stirling's approximation of n! (factorial of n)?", "answer"=>"2", "options"=>array("2.√(πn).(n/e)^n", "√(2πn).(n/e)^n", "√(πn).(n/e)^n", "√(πn).(n/e)^(n/2e)") ),
+    array("level"=>"Extreme","question"=>"Which of the following conditions is equivalent to A+B = A^B where '^' is XOR Bitwise operator?", "answer"=>"1", "options"=>array("A&B=0", "A|B=A&B", "A|B=0", "(~A)&(B|A)=0") ),
+    array("level"=>"Extreme","question"=>"Alice is using Django for her project and has a model called 'Product' which has a member variable decription. For ProdA the description is '<.ul><.li>Durable<./li><.li>3 Year warranty<./li><.li>Limited Stock<./li><./ul>'<br>But it is being printed as a string with tags instead of rendering as an HTML list. What is correct way to solve this issue?", "answer"=>"3", "options"=>array("{{ ProdA.description.HTML }}", "<.pre>{{ ProdA.description }}<./pre>", "{{ ProdA.description|safe }}", "{{ parseHTML(ProdA.description) }}") ),
+    array("level"=>"Extreme","question"=>"HTML:<br><.div class='a'><.div class='b' id='c'>Hello World<./div><./div><br>CSS:<br>.a{position:relative;}<br>.b{position:absolute; height:100%;}<br>#c{position:fixed; height:10px;}<br><br>What is the height of div with class 'A'?", "answer"=>"2", "options"=>array("10px", "0px", "100% of 16px = 16px", "10px+16px=26px") ),
+    array("level"=>"Extreme","question"=>"Which of the following properties is effective only when using display: flex?", "answer"=>"1", "options"=>array("align-items", "text-align", "float", "flex-template-row") ),
     array("level"=>"Extreme","question"=>"Which is not a Structured Query Language?", "answer"=>"3", "options"=>array("MySQL", "Postgres", "MongoDB", "MariaDB") ),
     array("level"=>"Extreme","question"=>"What does 'C' in 'MVC' stand for?", "answer"=>"2", "options"=>array("Command", "Controller", "Creator", "Catch") ),
     array("level"=>"Extreme","question"=>"Which of the following is not a framework of python?", "answer"=>"4", "options"=>array("Django", "Flask", "Hug", "Spring") ),
     array("level"=>"Extreme","question"=>"Which of the following is not a pattern matching Algorithm?", "answer"=>"4", "options"=>array("Boyer Moore", "Rabin Karp", "Knuth Morris Pratt", "Seive of Eratosthenes") )
 );
-//$ExtremeAnswers = array("2", "3", "2", "4", "4");
 
 
 ?>
