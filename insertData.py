f85e08ec8918a23c33855d18d6ff00a9c24cd4aa
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-07-02 03:06:40 +0530

    Added python script to make insertions in database
    We have to manually feed a list of names and it will generate a random set of scores following the rules that user has to clear previous level before moving on to next one.

---
+++ insertData.py
@@ -0,0 +1,20 @@
+from random import randint
+
+# names = ['James', 'John', 'Robert', 'Michael', 'William', 'David', 'Richard', 'Joseph', 'Thomas', 'Charles', 'Chris', 'Daniel', 'Matt', 'Anthony', 'Donald', 'Mark', 'Paul', 'Steven', 'Andrew', 'Kenneth', 'Joshua', 'George', 'Kevin', 'Brian', 'Edward', 'Ronald', 'Gary', 'Ash', 'Brock', 'Jason', 'Grant', 'Ryan', 'Eric', 'Laryy', 'Frank', 'Phineas', 'Ferb', 'Benjamin', 'Gregory', 'Sherlock', 'Sophie', 'Tailor', 'Aaron', 'Paul', 'Vaas', 'Lisa', 'Citra']
+names = ['Kyle', 'Kolin', 'Casey', 'Panda', 'Daisy', 'Mini', 'Shindeyuro', 'Lee', 'Shin', 'Elon', 'Rishabh', 'Raghav', 'Khushboo', 'Riya', 'Vaibhav', 'Kaustabh', 'Somil', 'Deedee', 'Oggy', 'Jill', 'Hoyt', 'Harsh', 'Priya', 'Shreya', 'Pappubhai', 'Munna', 'Pankaj']
+
+with open('sqlFile.txt', 'w') as sqlFile:
+    for i in range(len(names)):
+        username, easy, medium, hard, extreme = names[i], randint(6, 30)*5, 0, 0, 0
+        if easy >= 50:
+            medium = randint(6, 40)*5
+            if medium >= 100:
+                hard = randint(6, 50)*5
+                if hard >= 150:
+                    extreme = randint(6, 54)*5
+
+        score = easy + medium + hard + extreme
+
+        raw_statement = f"INSERT INTO `users` (`username`, `userpassword`, `progress`, `Score`, `MaxScoreEasy`, `MaxScoreMedium`, `MaxScoreHard`, `MaxScoreExtreme`) VALUES ('{username}', '123', '0', '{score}', '{easy}', '{medium}', '{hard}', '{extreme}');\n"
+        sqlFile.write(raw_statement)
+
