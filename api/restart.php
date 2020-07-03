e0e30a969407d826134d4fb2c45e815c46258740
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-07-03 20:12:03 +0530

    Set scores to zero when restart clicked

--- api/restart.php
+++ api/restart.php
@@ -12,6 +12,14 @@
     $user->progress = 0;
     $_SESSION['progress'] = 0;
 
+    $user->Score = 0;
+    $user->MaxScoreEasy = 0;
+    $user->MaxScoreMedium = 0;
+    $user->MaxScoreHard = 0;
+    $user->MaxScoreExtreme = 0;
+
+    $user->updateScore();
+
     if ($user->updateLevel()){
         header("Location: ../quiz.php");
         //echo "<br>Level Updated<br>";
